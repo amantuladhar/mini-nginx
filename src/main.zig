1323f6d595710e8d05db781a9563ac1006ac2479
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-05-30 15:04:23 -0500

    Echo server

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,96 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const gpa, const deinit = getAllocator();
+    defer _ = if (deinit) debug_allocator.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const server: [4]u8 = .{0} ** 4;
+    const server_port: u16 = 8080;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const server_fd = c.socket(posix.AF.INET, posix.SOCK.STREAM, 0);
+    var result = posix.errno(server_fd);
+    if (result != .SUCCESS) {
+        std.log.err("Call to socket failed: {any}", .{result});
+        std.process.exit(1);
+    }
+    defer _ = c.close(server_fd);
 
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    result = posix.errno(c.setsockopt(server_fd, posix.SOL.SOCKET, posix.SO.REUSEADDR, &@as(u32, 1), @sizeOf(u32)));
+    if (result != .SUCCESS) {
+        std.log.err("call to setsockopt failed: {any}", .{result});
+        std.process.exit(1);
+    }
+    const sock_addr_in: posix.sockaddr.in = .{
+        .family = posix.AF.INET,
+        .addr = @as(*align(1) const u32, @ptrCast(&server)).*,
+        .port = std.mem.nativeToBig(u16, server_port),
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    result = posix.errno(c.bind(server_fd, @ptrCast(&sock_addr_in), @sizeOf(@TypeOf(sock_addr_in))));
+    if (result != .SUCCESS) {
+        std.log.err("call to bin dfailed: {any}", .{result});
+        std.process.exit(1);
+    }
+
+    result = posix.errno(c.listen(server_fd, 1));
+    if (result != .SUCCESS) {
+        std.log.err("call to listen failed: {any}", .{result});
+        std.process.exit(1);
+    }
+
+    const existing_flag = c.fcntl(server_fd, posix.F.GETFL, @as(u32, 0));
+    result = posix.errno(existing_flag);
+    if (result != .SUCCESS) {
+        std.log.err("call to fcntl failed: {any}", .{result});
+        std.process.exit(1);
+    }
+
+    result = posix.errno(c.fcntl(server_fd, posix.F.SETFL, @as(usize, @intCast(existing_flag)) | posix.SOCK.NONBLOCK));
+    if (result != .SUCCESS) {
+        std.log.err("call to fcntl failed: {any}", .{result});
+        std.process.exit(1);
+    }
+
+    std.log.info("Waiting for client connection: \n", .{});
+
+    var client_addr: posix.sockaddr.in = undefined;
+    var client_addr_len: posix.socklen_t = @sizeOf(@TypeOf(client_addr));
+    const client_fd = c.accept(server_fd, @ptrCast(&client_addr), &client_addr_len);
+    result = posix.errno(client_fd);
+    if (result != .SUCCESS) {
+        std.log.err("call to accept failed: {any}", .{result});
+        std.process.exit(1);
+    }
+    defer _ = c.close(client_fd);
+    std.log.info("Connection from client accepted: {any}\n", .{client_addr});
+
+    const BUFFER_SIZE = 4096;
+    var buffer: [BUFFER_SIZE]u8 = .{0} ** BUFFER_SIZE;
+
+    const read_count = c.read(client_fd, buffer[0..BUFFER_SIZE].ptr, BUFFER_SIZE);
+    result = posix.errno(read_count);
+    if (result != .SUCCESS) {
+        std.log.err("call to read failed: {any}", .{result});
+        std.process.exit(1);
+    }
+    const reply = std.fmt.allocPrint(gpa, "Reply to: {s}", .{buffer[0..@intCast(read_count)]}) catch unreachable;
+    defer gpa.free(reply);
+
+    result = posix.errno(c.send(client_fd, reply.ptr, reply.len, 0));
+    if (result != .SUCCESS) {
+        std.log.err("call to send failed, {any}", .{result});
+        std.process.exit(1);
+    }
+}
+
+fn getAllocator() struct { Allocator, bool } {
+    return switch (builtin.mode) {
+        .Debug, .ReleaseSafe => .{ debug_allocator.allocator(), true },
+        .ReleaseFast, .ReleaseSmall => .{ debug_allocator.allocator(), true },
+    };
 }
 
 const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("mini_nginx_lib");
+const builtin = @import("builtin");
+const c = std.c;
+const posix = std.posix;
+const Allocator = std.mem.Allocator;
+const native_os = builtin.os.tag;
+var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
